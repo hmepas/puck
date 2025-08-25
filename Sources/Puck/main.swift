ac0f2af886f7842a3224017eee07ebe5098cc62b
Pavel Khmelinsky <hmepas@gmail.com>
2025-08-25 14:46:34 +0300

    chore(release): bump version to 0.1.10 in CLI

--- Sources/Puck/main.swift
+++ Sources/Puck/main.swift
@@ -8,7 +8,7 @@ struct Puck: ParsableCommand {
     static var configuration = CommandConfiguration(
         commandName: "puck",
         abstract: "A swift input method switching daemon for macOS",
-        version: "0.1.9"
+        version: "0.1.10"
     )
     
     @Flag(name: .shortAndLong, help: "List all available input sources")
